866e116d662e0f1c61a26b34107b582ba5f1ff0f
Krokas <rokas.kondrackis@gmail.com>
2024-09-08 20:15:06 +0300

    rot13 implementation

--- src/converters/StringConverter.php
+++ src/converters/StringConverter.php
@@ -4,19 +4,38 @@ namespace Converters;
 
 
 class StringConverter {
+    private static string $alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private static string $capitalizedAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public static function convertAlphabeticalPositions(string $string) : string
     {
         $result = "";
 
-        $alphabet = "abcdefghijklmnopqrstuvwxyz";
-
         for ($i = 0; $i < strlen($string); $i++) {
-            if (is_numeric($string[$i])) {
+            if (!ctype_alpha($string[$i])) {
                 $result .= $string[$i];
             } else {
-                $index = strpos($alphabet, strtolower($string[$i]), 0) + 1;
-                $prefix = $i > 0 ? "/" : "";
-                $result .= $prefix . $index;
+                $index = strpos(self::$alphabet, strtolower($string[$i]), 0) + 1;
+                $result .= "/" . $index;
+            }
+        }
+
+        return $result;
+    }
+
+    public static function convertRot13(string $string) : string
+    {
+        $result = "";
+
+        for ($i = 0; $i < strlen($string); $i++) {
+            if(!ctype_alpha($string[$i])) {
+                $result .= $string[$i];
+            } else {
+                $alphabet = ctype_upper($string[$i]) ? self::$capitalizedAlphabet : self::$alphabet;
+                $index = strpos($alphabet, $string[$i], 0);
+                $rot13Index = ($index + 13) % 26;
+
+                $result .= $alphabet[$rot13Index];
             }
         }
 
--- tests/StringConverterTest.php
+++ tests/StringConverterTest.php
@@ -5,10 +5,18 @@ use PHPUnit\Framework\TestCase;
 
 class StringConverterTest extends TestCase
 {
-    public function testConvertAlphabeticalPositionsShouldConvertStringCorrectly() : void
+    public function testConverterAlphabeticalPositionsShouldConvertStringCorrectly() : void
     {
         $convertedString = Converters\StringConverter::convertAlphabeticalPositions("22aAcd");
 
         $this->assertSame("22/1/1/3/4", $convertedString);
+    }
+
+    public function testConverterRot13ShouldConvertStringCorrectly() : void
+    {
+        $stringToConvert = "The Quick Brown Fox Jumps Over The Lazy Dog";
+        $convertedString = Converters\StringConverter::convertRot13($stringToConvert);
+
+        $this->assertSame(str_rot13($stringToConvert), $convertedString);
     }
 }
